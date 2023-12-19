cc4309b04f4741d026ae6a543fbfb465827c7885
Salxchange <138751618+Salxchange@users.noreply.github.com>
2023-12-19 22:55:27 +0100

    Update torrent_info.py

--- torrentp/torrent_info.py
+++ torrentp/torrent_info.py
@@ -4,7 +4,7 @@ class TorrentInfo:
     def __init__(self, save_path, lt):
         self._path = save_path
         self._lt = lt
-        self._info = self._lt.torrent_info(save_path)
+        self._info = self._lt.torrent_info(save_path)  # Assuming lt has a method torrent_info to get info
 
     def show_info(self):
         print("Torrent Information:")
@@ -12,8 +12,8 @@ class TorrentInfo:
         print(f"Info: {self._info}")
 
 # Example usage
-if __name__ == "__main__":
-    # Assuming lt is an instance of a Torrent object
+if __name__ == "__main__":  # Fixed name == "main" to __name__ == "__main__"
+    # Assuming lt is an instance of a Torrent object (which needs to be defined or imported)
     lt = Torrent()  # Create or obtain the Torrent object
     save_path = "path/to/torrent/file.torrent"
     torrent = TorrentInfo(save_path, lt)
