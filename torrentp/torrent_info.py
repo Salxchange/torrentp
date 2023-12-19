373718382be173b102a60f39588f93d80ef54ca9
Salxchange <138751618+Salxchange@users.noreply.github.com>
2023-12-19 22:36:56 +0100

    Update torrent_info.py

--- torrentp/torrent_info.py
+++ torrentp/torrent_info.py
@@ -1,19 +1,39 @@
-class TorrentInfo:
-    def __init__(self, save_path, lt):
-        self._path = save_path
+from .session import Session
+from .torrent_info import TorrentInfo
+from .downloader import Downloader
+import libtorrent as lt
+
+
+class TorrentDownloader:
+    def __init__(self, file_path, save_path):
+        self._file_path = file_path
+        self._save_path = save_path
+        self._downloader = None
+        self._torrent_info = None
         self._lt = lt
-        self._info = self._lt.torrent_info(save_path)
+        self._file = None
+        self._add_torrent_params = None
+        self._session = Session(self._lt)
 
-    def show_info(self):
-        print("Torrent Information:")
-        print(f"Path: {self._path}")
+    def start_download(self):
+        if self._file_path.startswith('magnet:'):
+            self._add_torrent_params = self._lt.parse_magnet_uri(self._file_path)
+            self._add_torrent_params.save_path = self._save_path
+            self._downloader = Downloader(session=self._session(), torrent_info=self._add_torrent_params,
+                                          save_path=self._save_path, libtorrent=lt, is_magnet=True)
+        else:
+            self._torrent_info = TorrentInfo(self._file_path, self._lt)
+            self._downloader = Downloader(session=self._session(), torrent_info=self._torrent_info(),
+                                          save_path=self._save_path, libtorrent=None, is_magnet=False)
 
-    def create_torrent_info(self):
-        self._info = self._lt.torrent_info(self._path)
-        return self._info
+        self._file = self._downloader
+        return self._file.download()
 
     def __str__(self):
-        return f"TorrentInfo: Path={self._path}, Info={self._info}"
+        pass
 
     def __repr__(self):
-        return f"TorrentInfo({self._path}, {self._lt})"
+        pass
+
+    def __call__(self):
+        pass
