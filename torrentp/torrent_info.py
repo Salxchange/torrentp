d3da66c106ec48d321377e25c13e6c66dac09367
Salxchange <138751618+Salxchange@users.noreply.github.com>
2023-12-19 22:41:14 +0100

    Update torrent_info.py

--- torrentp/torrent_info.py
+++ torrentp/torrent_info.py
@@ -1,33 +1,15 @@
-from .session import Session
-from .torrent_info import TorrentInfo
-from .downloader import Downloader
-import libtorrent as lt
+class TorrentInfo:
+    def __init__(self, path, libtorrent):
+        self._path = save_path
+        self._lt = libtorrent
+        self._info = self._lt.torrent_info(self._path)
 
+    def show_info(self):
+        pass
 
-class TorrentDownloader:
-    def __init__(self, file_path, save_path):
-        self._file_path = file_path
-        self._save_path = save_path
-        self._downloader = None
-        self._torrent_info = None
-        self._lt = lt
-        self._file = None
-        self._add_torrent_params = None
-        self._session = Session(self._lt)
-
-    def start_download(self):
-        if self._file_path.startswith('magnet:'):
-            self._add_torrent_params = self._lt.parse_magnet_uri(self._file_path)
-            self._add_torrent_params.save_path = self._save_path
-            self._downloader = Downloader(session=self._session(), torrent_info=self._add_torrent_params,
-                                          save_path=self._save_path, libtorrent=lt, is_magnet=True)
-        else:
-            self._torrent_info = TorrentInfo(self._file_path, self._lt)
-            self._downloader = Downloader(session=self._session(), torrent_info=self._torrent_info(),
-                                          save_path=self._save_path, libtorrent=None, is_magnet=False)
-
-        self._file = self._downloader
-        return self._file.download()
+    def create_torrent_info(self):
+        self._info = self._lt.torrent_info(self._path)
+        return self._info
 
     def __str__(self):
         pass
@@ -36,4 +18,4 @@ class TorrentDownloader:
         pass
 
     def __call__(self):
-        pass
+        return self.create_torrent_info()
