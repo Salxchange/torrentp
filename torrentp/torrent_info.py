97f80959c2da534aedb6c0622cb458574c899acf
Salxchange <138751618+Salxchange@users.noreply.github.com>
2023-12-19 22:02:43 +0100

    Update torrent_info.py

--- torrentp/torrent_info.py
+++ torrentp/torrent_info.py
@@ -5,17 +5,22 @@ class TorrentInfo:
         self._info = self._lt.torrent_info(self._path)
 
     def show_info(self):
-        pass
+        # Implement the logic to display torrent information here
+        print("Torrent Information:")
+        print(f"Path: {self._path}")
+        # Add more attributes as needed
 
     def create_torrent_info(self):
         self._info = self._lt.torrent_info(self._path)
         return self._info
-
+    
     def __str__(self):
-        pass
-
+        # Implement the logic to return a string representation of the object
+        return f"TorrentInfo: Path={self._path}, Info={self._info}"
+        
     def __repr__(self):
-        pass
+        # Implement the logic to return a string representation of the object for debugging
+        return f"TorrentInfo({self._path}, {self._lt})"
 
-    def __call__(self):
+    def call(self):
         return self.create_torrent_info()
